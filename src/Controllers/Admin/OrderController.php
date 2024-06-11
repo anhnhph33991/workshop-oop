0e3611451d2f417a576e7c83208211ca75821f0a
Hoang Anh <hoang838604@gmail.com>
2024-06-12 05:46:49 +0700

    luxchill: End

--- src/Controllers/Admin/OrderController.php
+++ src/Controllers/Admin/OrderController.php
@@ -17,6 +17,7 @@ class OrderController extends Controller implements InterfaceCrudController
 
 	public function __construct()
 	{
+		parent::__construct();
 		$this->order = new Order();
 		$this->orderDetail = new OrderDetail();
 		$this->product = new Product();
@@ -28,17 +29,17 @@ class OrderController extends Controller implements InterfaceCrudController
 //		$selectAllOrderDetail  = $this->orderDetail->getAll("*");
 		$page = $_GET['page'] ?? 1;
 		[$orders, $totalPage] = $this->orderDetail->paginateAbc($page, 10);
-		
+
 //		echo "<pre>";
 //		print_r($orders);
 //		echo "</pre>";
 //		die;
 
-	$data = [
-		'orders' => $orders,
-		'totalPage' => $totalPage,
-		'page' => $page
-	];
+		$data = [
+			'orders' => $orders,
+			'totalPage' => $totalPage,
+			'page' => $page
+		];
 
 		return $this->renderAdmin($this->folder . __FUNCTION__, $data);
 	}
@@ -60,12 +61,79 @@ class OrderController extends Controller implements InterfaceCrudController
 
 	public function edit($id)
 	{
-		return $this->renderAdmin($this->folder . __FUNCTION__);
+		$order = $this->orderDetail->findOne($id);
+//		echo "<pre>";
+//		print_r($order);
+//		echo "</pre>";
+//		die;
+
+		$_SESSION['order_id'] = $order['order_id'];
+		$data = [
+			'orders' => $order
+		];
+
+
+//		echo "<pre>";
+//		print_r($_SESSION['order_id']);
+//		echo "</pre>";
+
+
+		return $this->renderAdmin($this->folder . __FUNCTION__, $data);
 	}
 
 	public function update($id)
 	{
+		$order = $this->order->getOne($id);
+		$idOld = $_SESSION['order_id'];
 
+
+		echo "<pre>";
+		print_r($idOld);
+		echo "</pre>";
+
+		$validation = $this->validator->make($_POST, [
+			'status' => 'required'
+		]);
+
+		$validation->validate();
+		if ($validation->fails()) {
+			$err = $validation->errors()->firstOfAll();
+
+			echo "<pre>";
+			print_r($err);
+			echo "</pre>";
+		} else {
+			$data = [
+				'status' => $_POST['status'] ?? $order['od_id'],
+			];
+			
+			echo "<pre>";
+			print_r($data);
+			echo "</pre>";
+			
+			
+
+//			echo "<pre>";
+//			print_r($data);
+//			echo "</pre>";
+
+//			echo "<pre>";
+//			print_r($data);
+//			echo "</pre>";
+//
+//			echo $idOld;
+//			echo "</br>";
+//			echo $id;
+
+//			$this->order->update($id, $data);
+			$this->order->update($idOld, $data);
+			setToastr('Update order success', 'success');
+			header('location: ' . routeAdmin("orders/{$id}/edit"));
+
+//			echo "<pre>";
+//			print_r($data);
+//			echo "</pre>";
+		}
 	}
 
 	public function delete($id)
--- src/Views/Admin/orders/edit.blade.php
+++ src/Views/Admin/orders/edit.blade.php
@@ -1,93 +1,113 @@
 @extends('layouts.master')
 @section('title', 'Create Category')
 @section('main')
-<section class="section">
-    <div class="section-header">
-        <div class="section-header-back">
-            <a href="{{ routeAdmin('orders') }}" class="btn btn-icon"><i class="fas fa-arrow-left"></i></a>
+    <section class="section">
+        <div class="section-header">
+            <div class="section-header-back">
+                <a href="{{ routeAdmin('orders') }}" class="btn btn-icon"><i class="fas fa-arrow-left"></i></a>
+            </div>
+            <h1>Edit New Order</h1>
+            <div class="section-header-breadcrumb">
+                <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
+                <div class="breadcrumb-item"><a href="#">Orders</a></div>
+                <div class="breadcrumb-item">Edit New Orders</div>
+            </div>
         </div>
-        <h1>Edit New Order</h1>
-        <div class="section-header-breadcrumb">
-            <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
-            <div class="breadcrumb-item"><a href="#">Orders</a></div>
-            <div class="breadcrumb-item">Edit New Orders</div>
-        </div>
-    </div>
 
-    <div class="section-body">
-        <h2 class="section-title">Edit New Order</h2>
-        <p class="section-lead">
-            On this page you can Edit a new Order and fill in all fields.
-        </p>
+        <div class="section-body">
+            <h2 class="section-title">Edit New Order</h2>
+            <p class="section-lead">
+                On this page you can Edit a new Order and fill in all fields.
+            </p>
 
-        <div class="row">
-            <div class="col-12">
-                <div class="card">
-                    <div class="card-header">
-                        <h4>Write Your Order</h4>
-                    </div>
-                    <div class="card-body">
-                        <div class="form-group row mb-4">
-                            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Title</label>
-                            <div class="col-sm-12 col-md-7">
-                                <input type="text" class="form-control">
-                            </div>
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4>Write Your Order</h4>
                         </div>
-                        <div class="form-group row mb-4">
-                            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Category</label>
-                            <div class="col-sm-12 col-md-7">
-                                <select class="form-control selectric">
-                                    <option>Tech</option>
-                                    <option>News</option>
-                                    <option>Political</option>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="form-group row mb-4">
-                            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Content</label>
-                            <div class="col-sm-12 col-md-7">
-                                <textarea class="summernote-simple"></textarea>
-                            </div>
-                        </div>
-                        <div class="form-group row mb-4">
-                            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Thumbnail</label>
-                            <div class="col-sm-12 col-md-7">
-                                <div id="image-preview" class="image-preview">
-                                    <label for="image-upload" id="image-label">Choose File</label>
-                                    <input type="file" name="image" id="image-upload" />
+
+                        @php
+//                        echo "<pre>";
+//                        print_r($orders);
+//                        echo "</pre>";
+							$images = explode(',', $orders['p_image']);
+
+                            switch ($orders['o_status']){
+                                case "0":
+                                    $status = "Cho Xac Nhan";
+                                    break;
+                                    case "1":
+                                        $status = "Da Xac Nhan";
+                                    break;
+                                    case "2":
+                                        $status = "Dang Chuan Bi Hang";
+                                    break;
+                                    case "3":
+                                        $status = "Dang Giao Hang";
+                                    break;
+                                    case "4":
+                                        $status = "Da Giao Hang";
+                                    break;
+                                    case "5":
+                                        $status = "Da Huy";
+                                    break;
+                                    case "6":
+                                        $status = "Hoan Hang";
+                                    break;
+
+                            }
+
+                        @endphp
+
+                        <form action="{{ routeAdmin("orders/{$orders['od_id']}/update") }}" method="post" class="card-body">
+                            <div class="form-group row mb-4">
+                                <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Product
+                                    Name</label>
+                                <div class="col-sm-12 col-md-7">
+                                    <input type="text" class="form-control" value="{{ $orders['od_productName'] }}"
+                                           disabled>
                                 </div>
                             </div>
-                        </div>
-                        <div class="form-group row mb-4">
-                            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Tags</label>
-                            <div class="col-sm-12 col-md-7">
-                                <input type="text" class="form-control inputtags">
+                            <div class="form-group row mb-4">
+                                <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Price</label>
+                                <div class="col-sm-12 col-md-7">
+                                    <input type="text" class="form-control"
+                                           value="{{ number_format(($orders['od_priceOffer'] ?: $orders['od_price']) * $orders['od_qty']) }}"
+                                           disabled>
+                                </div>
                             </div>
-                        </div>
-                        <div class="form-group row mb-4">
-                            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Status</label>
-                            <div class="col-sm-12 col-md-7">
-                                <select class="form-control selectric">
-                                    <option>Publish</option>
-                                    <option>Draft</option>
-                                    <option>Pending</option>
-                                </select>
+                            <div class="form-group row mb-4">
+                                <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Status</label>
+                                <div class="col-sm-12 col-md-7">
+                                    <select class="form-control selectric" name="status">
+                                        <option value="{{ $order['o_status'] }}" disabled selected>{{ $status }}</option>
+                                        <option value="0">Cho Xac Nhan</option>
+                                        <option value="1">Da Xac Nhan</option>
+                                        <option value="2">Dang Chuan Bi Hang</option>
+                                        <option value="3">Dang Giao Hang</option>
+                                        <option value="4">Da Giao Hang</option>
+                                        <option value="5">Da Huy</option>
+                                        <option value="6">Hoan Hang</option>
+                                    </select>
+                                </div>
                             </div>
-                        </div>
-                        <div class="form-group row mb-4">
-                            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3"></label>
-                            <div class="col-sm-12 col-md-7">
-                                <button class="btn btn-primary">Create Post</button>
+
+
+                            <div class="form-group row mb-4">
+                                <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3"></label>
+                                <div class="col-sm-12 col-md-7">
+                                    <button class="btn btn-primary">Edit Order</button>
+                                </div>
                             </div>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
 @endsection
 
 @section('javascript')
-<script src="{{ asset('admin/js/page/features-post-create.js') }}"></script>
+    <script src="{{ asset('admin/js/page/features-post-create.js') }}"></script>
 @endsection
