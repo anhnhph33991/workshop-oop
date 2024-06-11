79ad2050e2ef963b2d97137711612fd119254aaa
HoangAnh <160491033+anhnhph33991@users.noreply.github.com>
2024-06-12 05:51:55 +0700

    format l1

--- routes/client.php
+++ routes/client.php
@@ -19,9 +19,9 @@ $router->get('shops', ShopController::class . '@index');
 //$router->get('shop/{id}', ShopController::class . '@detail');
 
 $router->mount('/shop', function () use ($router) {
-	$router->get('/{slug}/comment', ShopController::class . '@comment');
+	$router->get('/{slug}/comment', 	ShopController::class . '@comment');
 	$router->post('/{slug}/handle-comment', ShopController::class . '@handleComment');
-	$router->get('/{slug}', ShopController::class . '@detail');
+	$router->get('/{slug}', 		ShopController::class . '@detail');
 });
 
 
@@ -34,26 +34,26 @@ $router->get('/products/{id}', function ($id) {
 	echo "Page product detail = " . $id;
 });
 
-$router->get('/cart', CartController::class . '@index');
-$router->post('/cart/add', CartController::class . '@add');
-$router->post('/cart/update', CartController::class . '@update');
-$router->get('/cart/delete/{id}', CartController::class . '@delete');
+$router->get('/cart', 				CartController::class . '@index');
+$router->post('/cart/add', 			CartController::class . '@add');
+$router->post('/cart/update', 			CartController::class . '@update');
+$router->get('/cart/delete/{id}', 		CartController::class . '@delete');
 
 
-$router->get('/check-out', CheckOutController::class . '@index');
-$router->post('/check-out/add', CheckOutController::class . '@add');
-$router->get('/check-out/momo', CheckOutController::class . '@handleMomo');
+$router->get('/check-out', 			CheckOutController::class . '@index');
+$router->post('/check-out/add', 		CheckOutController::class . '@add');
+$router->get('/check-out/momo', 		CheckOutController::class . '@handleMomo');
 
 
-$router->get('/confirm', CheckOutController::class . '@confirm');
+$router->get('/confirm', 			CheckOutController::class . '@confirm');
 
-$router->get('/track-order', OrderController::class . '@check');
+$router->get('/track-order', 			OrderController::class . '@check');
 
-$router->get('/my-orders', OrderController::class . '@myOrder');
-$router->get('/my-orders/{id}/delete', OrderController::class . '@delete');
-$router->get('/my-orders/{id}', OrderController::class . '@detail');
+$router->get('/my-orders', 			OrderController::class . '@myOrder');
+$router->get('/my-orders/{id}/delete', 		OrderController::class . '@delete');
+$router->get('/my-orders/{id}',			OrderController::class . '@detail');
 
-$router->get('/account', AccountController::class . '@index');
+$router->get('/account', 			AccountController::class . '@index');
 
 $router->get('/profile', function () {
 	echo "Page profile";
@@ -61,15 +61,15 @@ $router->get('/profile', function () {
 
 // auth route
 
-$router->get('/login', AuthController::class . '@login');
+$router->get('/login', 			AuthController::class . '@login');
 
-$router->post('/handle-login', AuthController::class . '@handleLogin');
+$router->post('/handle-login', 		AuthController::class . '@handleLogin');
 
-$router->get('/register', AuthController::class . '@register');
+$router->get('/register', 		AuthController::class . '@register');
 
-$router->post('/handle-register', AuthController::class . '@handleRegister');
+$router->post('/handle-register', 	AuthController::class . '@handleRegister');
 
-$router->get('/logout', AuthController::class . '@logout');
+$router->get('/logout', 		AuthController::class . '@logout');
 
 
 $router->get('/forgot-password', function () {
